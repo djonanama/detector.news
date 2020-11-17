8ce369285f1fd6fbeb62af18bfb39a3610762eab
Djonanama <djonanama@gmail.com>
2020-11-17 12:13:03 +0800

    main

--- deploy.sh
+++ deploy.sh
@@ -3,21 +3,25 @@
 set -e
 set -u
 
-sudo docker stack deploy --with-registry-auth -c docker-compose.yaml detector
+cp -f -R  ".env.[PROD]" ".env"
+docker stack deploy --with-registry-auth -c docker-compose.yaml detector
 
 service=detector_webserver-detector 
-cp -f -R  ".env.[PROD]" ".env"
-
 serviceID=$(sudo docker service ps -f name=$service -f desired-state=running $service -q --no-trunc |head -n1)
 node=$(sudo docker service ps -f name=$service -f desired-state=running $service --format="{{.Node}}"| head -n1 )
 
+echo "----------------------"
+echo "serviceID:$serviceID"
+echo "node:$node"
+echo "service:$service"
+echo "----------------------"
 yarn install
 
 docker service scale detector_backend-node-blue=0
 yarn build
 docker service scale detector_backend-node-blue=1
 
-until curl http://localhost:5002; do
+until curl -sS http://localhost:5002; do
   >&2 echo "backend-node-blue - sleeping"
   sleep 3s
 done 
@@ -30,7 +34,7 @@ cp -f -R  "./common/blue/dist" "./common/green/"
 
 docker service scale detector_backend-node-green=1
 
-until curl http://localhost:5001; do
+until curl -sS http://localhost:5001; do
   >&2 echo "backend-node-green - sleeping"
   sleep 1s
 done 
@@ -38,4 +42,3 @@ done
 sudo docker -H $node exec -it $service".1."$serviceID sh -c "/src/nginx/nginx-conf/switch backend-node-green"
 
 docker service scale detector_backend-node-blue=0
-
