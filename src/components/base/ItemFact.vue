443f2331118e072e3f8bc934cab1f6e8fd90aae7
Djonanama <djonanama@gmail.com>
2020-11-26 17:28:27 +0800

    main

--- src/components/base/ItemFact.vue
+++ src/components/base/ItemFact.vue
@@ -1,12 +1,12 @@
 <template>
   <mdb-container class="mt-3 mb-3">
-    <mdb-card :class="islink ? 'hoverable' : ''">
+    <mdb-card :class="islink ? 'hoverable mr-2' : 'mr-2'">
       <mdb-view hover>
         <div class="row">
           <div class="col-md-3 mt-auto mb-auto">
             <mdb-card-body class="ml-0 mr-0 text-center">
               <mdb-avatar
-                class="white my-0 py-0"
+                class="white my-0 py-0 mb-2"
                 style="display: block;margin-left: auto;margin-right: auto;"
               >
                 <img
@@ -14,13 +14,14 @@
                   class="rounded-circle img-fluid"
                 />
               </mdb-avatar>
-              <h5 class="pt-2">{{ wire.agent_val.title }}</h5>
+              <strong>{{ wire.agent_val.title }}</strong>
+              <!-- <h5 class="pt-2">{{ wire.agent_val.title }}</h5> -->
             </mdb-card-body>
           </div>
 
-          <div class="col-md-9 mt-4 mb-auto">
-            <div class="row mr-1">
-              <div class="col-md-8">
+          <div class="col-md-8 mt-4 mb-auto">
+            <div class="row">
+              <div class="col-sm-8 col-md-9 col-lg-10 mr-0">
                 <mdb-card-body class="p-0" v-if="islink == false">
                   <p class="dark-grey-text blockquote bq-primary">
                     <mdb-icon icon="quote-left" class="pr-2"></mdb-icon>
@@ -48,7 +49,7 @@
                 </p>
               </div>
 
-              <div class="col-md-4 mt-auto mb-auto ml-auto mr-auto pr-0 pl-0">
+              <div class="col-sm-4 col-md-3 col-lg-2 mt-auto mb-auto ml-auto mr-auto pr-0 pl-0">
                 <div
                   class="text-center ml-0 mr-0 pl-0 pr-0"
                   :style="color[wire.truth_val.truth_id]"
